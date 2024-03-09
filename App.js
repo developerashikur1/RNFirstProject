44822f036005af902c3f95844aadb107607be0c1
developerashikur1 <masik4506@gmail.com>
2024-03-09 17:46:38 +0600

    my first native app

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [textState, setTextState] = useState('');
+    const [allStates, setAllStates] = useState([]);
+
+    function handleTextInput (textFromInput) {
+        setTextState(textFromInput);
+    }
+
+    function handleClickButton () {
+        setAllStates((currentState) => [...currentState, textState]);
+    }
+    
+    console.log(allStates)
+    return (
+        <View style={styles.container}>
+            <View style={styles.headerContainer}>
+                <TextInput style={styles.textInput} placeholder='Search input' onChangeText={handleTextInput} />
+                <Button title='Click Me' onPress={handleClickButton} />
+            </View>
+            <View style={styles.bodyContainer}>
+                {
+                    allStates.map((state, index) => <Text style={[index % 2 === 0 ? styles.individualStyle : styles.individualStyle1, styles.commonStyle]} key={state}>{state}</Text>)
+                }
+                {/* <StatusBar style="auto" /> */}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        // minHeight: '100vh',
+        color: '#fff',
+        // backgroundColor:"green",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        borderWidth: 1,
+        borderRadius: 0,
+        padding: 2.5,
+        width: '70%',
+        marginRight: 8,
+    },
+    headerContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        // borderColor: 'red',
+    },
+    bodyContainer: {
+        flex: 5,
+        borderTopWidth: 1,
+        padding: 15,
+        width: '100%',
+        gap: 10,
+    },    
+    commonStyle:{
+        paddingVertical: 8,
+        paddingHorizontal: 10,
+        fontSize: 18,
+        textTransform: 'capitalize',
+        fontWeight: 400,
+    },
+    individualStyle:{
+        backgroundColor: 'orange',
+    },
+    individualStyle1:{
+        backgroundColor: '#cccccc',
+    },
 });
